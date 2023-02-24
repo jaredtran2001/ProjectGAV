3f69821c5e69c18ca4ee2156c8226de31760872b
Jared Tran <jtran9@cs.washington.edu>
2023-02-23 20:33:40 -0800

    created mvp of screens and game logic

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { Component, React } from 'react';
 
-export default function App() {
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/Home';
+import SelectionScreen from'./screens/Selection';
+import GameScreen from './screens/Game';
+import LoadScreen from './screens/Load';
+import ResultsScreen from './screens/Results';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen 
+          name="Selection" 
+          component={SelectionScreen} 
+          options={{title: 'Selection'}}
+        />
+        <Stack.Screen 
+          name="Game" 
+          component={GameScreen} 
+          options={{title: 'Game'}}
+        />
+        <Stack.Screen 
+          name="Load" 
+          component={LoadScreen} 
+          options={{title: 'Loading'}}
+        />
+        <Stack.Screen 
+          name="Results" 
+          component={ResultsScreen} 
+          options={{title: 'Results'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <View style={styles.container}>
+//         <Header/>
+//         <PlayButton/>
+//       </View>
+//     </NavigationContainer>
+    
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 60,
+//     alignItems: 'center',
+//   },
+//   container2: {
+//     flex: 2,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   }
+// });
