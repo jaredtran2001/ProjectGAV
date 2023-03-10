c3616dfc516f7d0bd0db26fc01f46d64d0f2eb86
Jared Tran <jtran9@cs.washington.edu>
2023-03-09 21:00:04 -0800

    Made game screen generic and word set is passed in before playing and play again makes sure words aren't repeated

--- screens/Selection.js
+++ screens/Selection.js
@@ -3,12 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import PlayButton from '../components/PlayButton';
 
 const Selection = ({navigation}) => {
+  
+  function generateFirstWord(wordSet){
+    let items = Array.from(wordSet);
+    let word = items[Math.floor(Math.random() * items.length)];
+    wordSet.delete(word);
+    console.log(word);
+    return word;
+  }
+
+  //Initialize Sets
+  let animeSet = new Set();
+  let animalsSet = new Set();
+  let kPopSet = new Set();
+  let pokemonSet = new Set();
+  
+  //Fill in Sets
+  animeSet.add("one piece").add("naruto").add("blue lock").add("hunterxhunter").add("AOT").add("Dragon Ball Z").add("SpyxFamily").add("Haikyu").add("My Hero Academia").add("JoJo's Bizarre Adventure").add("Jujutsu Kaisen").add("Chainsaw Man").add("Shield Hero").add("Full Metal Alchemist");
+  animalsSet.add("raccoon").add("tiger").add("swordfish").add("lion").add("hippo").add("giraffe").add("spider").add("alligator").add("turtle").add("rabbit").add("crow").add("snake");
+  kPopSet.add("G-Dragon").add("Jay Park").add("Jisoo").add("Jennie").add("Jin").add("Jimin").add("Suga").add("V").add("Chen").add("Taeyang").add("Hyojung").add("Mimi").add("Yubin");
+  pokemonSet.add("Pikachu").add("Eevee").add("Charizard").add("Snorlax").add("Gyarados").add("Meowth").add("Raichu").add("Mewtwo").add("Psyduck");
+  
+  //Create first words
+  let firstAnimeWord = generateFirstWord(animeSet);
+  let firstAnimalWord = generateFirstWord(animalsSet);
+  let kPopFirstWord = generateFirstWord(kPopSet);
+  let pokemonFirstWord = generateFirstWord(pokemonSet);
   return (
     <View style={styles.container}>
-        <PlayButton onPress = {() => navigation.navigate('Load')} title = 'Anime'/>
-        <PlayButton onPress = {() => navigation.navigate('Game')} title = 'Animals'/>
-        <PlayButton onPress = {() => navigation.navigate('Game')} title = 'Food'/>
-        <PlayButton onPress = {() => navigation.navigate('Game')} title = 'Alcohol'/>
+        <PlayButton onPress = {() => navigation.navigate('Load', {set: animeSet, firstWord: firstAnimeWord})} title = 'Anime'/>
+        <PlayButton onPress = {() => navigation.navigate('Load', {set: animalsSet, firstWord: firstAnimalWord})} title = 'Animals'/>
+        <PlayButton onPress = {() => navigation.navigate('Load', {set: kPopSet, firstWord: kPopFirstWord})} title = 'K-Pop Stars'/>
+        <PlayButton onPress = {() => navigation.navigate('Load', {set: pokemonSet, firstWord: pokemonFirstWord} )} title = 'Pokemon'/>
     </View>
     
   );
